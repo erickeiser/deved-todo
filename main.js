810d34642389e2d76e3ebffbb6269bedb654745f
Eric Keiser <ekeiser@ekeiser.lan.tombras.com>
2020-12-17 11:38:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+const todos = document.querySelector(".todo");
+// event listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+// functions
+function addTodo(event) {
+  event.preventDefault();
+
+  // Create todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class='fa fa-check'></i>";
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // trash can  button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "<i class='fa fa-trash'></i>";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Append to list
+  todoList.appendChild(todoDiv);
+
+  // Clear input value on submit
+  todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  const item = event.target;
+
+  // Delete Todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    // wait till animation ends before deleting todo
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  // Check Mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    //     console.log(todo);
+  }
+}
+
+// Creae a filter to see 'completed', 'uncompleted' and all
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+        case "uncompleted":
+              if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+              } else {
+                    todo.style.display = "none"
+              }
+              break;
+    }
+  });
+}
+
+
